c57cb4b5f0a332a07978c2516e35245d40738e71
JVitor22 <jvfaria015@gmail.com>
2020-11-07 16:52:02 -0300

    eager loading query

--- desafio-backend-node/app/Controllers/Http/ProductController.js
+++ desafio-backend-node/app/Controllers/Http/ProductController.js
@@ -5,10 +5,16 @@ const Image = use('App/Models/Image')
 class ProductController {
   async index ({ request, response, view }) {
     try {
-      const products = Product.all()
+      const products = await Product
+        .query()
+        .with('category')
+        .fetch()
 
-      return products
+      return response
+        .status(200)
+        .json(products)
     } catch (error) {
+      console.log(error)
       return response.status(400).send({ message: { error: 'an error has occurred' } })
     }
   }
@@ -36,16 +42,29 @@ class ProductController {
     }
   }
 
-  async create ({ request, response }) {
-  }
-
-  async show ({ params, request, response, view }) {
-  }
-
-  async edit ({ params, request, response, view }) {
-  }
-
   async update ({ params, request, response }) {
+    try {
+      const product = await Product.findOrFail(params.id)
+      const data = request.only(['name', 'image_file_name', 'category_name', 'price'])
+      const category = await Category.findBy('name', data.category_name)
+      if (!category) {
+        return response.status(400).send({ message: { error: 'A product must belong to a category!' } })
+      }
+      const image = await Image.findBy('file_name', data.image_file_name)
+      product.merge({
+        name: data.name,
+        stock_balance: product.stock_balance,
+        image_id: image ? image.id : null,
+        category_id: category.id,
+        price: data.price
+      })
+      await product.save()
+
+      return product
+    } catch (error) {
+      console.log(error)
+      return response.status(400).json({ error: { message: 'Product id not founded!' } })
+    }
   }
 
   async destroy ({ params, response }) {
@@ -59,6 +78,47 @@ class ProductController {
       return response.status(200).send({ message: 'Product deleted' })
     } catch (error) {
       return response.status(404).send({ error: { message: 'Category id not founded!' } })
+    }
+  }
+
+  // Custom endpoints
+  async addBalance ({ request, response, params }) {
+    try {
+      const product = await Product.findOrFail(params.id)
+      product.stock_balance += 1
+      await product.save()
+
+      return response.status(200).json({
+        message: 'Stock updated !',
+        product
+      })
+    } catch (error) {
+      console.log(error)
+      return response.status(400).json({ error: { message: 'Product id not founded' } })
+    }
+  }
+
+  async removeBalance ({ request, response, params }) {
+    try {
+      const product = await Product.findOrFail(params.id)
+      if (product.stock_balance === 0 || (product.stock_balance - 1) < 0) {
+        return response.status(400).json({
+          error: {
+            message: 'Stock balance cannot be less than 0'
+          }
+        })
+      }
+
+      product.stock_balance -= 1
+      await product.save()
+
+      return response.status(200).json({
+        message: 'Stock updated !',
+        product
+      })
+    } catch (error) {
+      console.log(error)
+      return response.status(400).json({ error: { message: 'Product id not founded' } })
     }
   }
 }
