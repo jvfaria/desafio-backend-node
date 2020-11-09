ee4fabea8ece4600b72602408032059d43302fb2
JVitor22 <jvfaria015@gmail.com>
2020-11-08 22:37:11 -0300

    routes auth updated

--- desafio-backend-node/start/routes.js
+++ desafio-backend-node/start/routes.js
@@ -2,33 +2,36 @@
 
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
-
-Route.get('/', () => {
-  return { greeting: 'Hello world in JSON' }
-})
-
-Route.get('/users/:key?', 'UserController.index')
-Route.post('/users', 'UserController.store')
-Route.delete('/users/:id', 'UserController.destroy')
-Route.put('/users/:id', 'UserController.update')
-
 Route.post('/auth/login', 'AuthController.store')
-Route.post('/auth/logout', 'AuthController.destroy')
-
+Route.post('/users', 'UserController.store')
+Route.get('/users/:key?', 'UserController.index')
 Route.post('/forgot', 'RecoverPasswordController.store')
 Route.put('/forgot', 'RecoverPasswordController.update')
 
-Route.get('/categories/:key?', 'CategoryController.index')
-Route.post('/categories', 'CategoryController.store')
-Route.put('/categories/:id', 'CategoryController.update')
-Route.delete('/categories/:id', 'CategoryController.destroy')
+Route.group(() => {
+  Route.post('/auth/logout', 'AuthController.destroy')
 
-Route.get('/products/:key?', 'ProductController.index')
-Route.post('/products', 'ProductController.store')
-Route.post('/products/:id/balance', 'ProductController.addBalance')
-Route.put('/products/:id', 'ProductController.update')
-Route.delete('/products/:id', 'ProductController.destroy')
-Route.delete('/products/:id/balance', 'ProductController.removeBalance')
+  Route.get('/products/:key?', 'ProductController.index')
 
-Route.get('/images', 'ImageController.index')
-Route.post('/images', 'ImageController.store')
+  Route.get('/categories/:key?', 'CategoryController.index')
+
+  Route.post('/sale', 'SaleController.create')
+}).middleware(['auth'])
+
+Route.group(() => {
+  Route.post('/products', 'ProductController.store')
+  Route.post('/products/:id/balance', 'ProductController.addBalance')
+  Route.put('/products/:id', 'ProductController.update')
+  Route.delete('/products/:id', 'ProductController.destroy')
+  Route.delete('/products/:id/balance', 'ProductController.removeBalance')
+
+  Route.post('/categories', 'CategoryController.store')
+  Route.put('/categories/:id', 'CategoryController.update')
+  Route.delete('/categories/:id', 'CategoryController.destroy')
+
+  Route.delete('/users/:id', 'UserController.destroy')
+  Route.put('/users/:id', 'UserController.update')
+
+  Route.get('/images', 'ImageController.index')
+  Route.post('/images', 'ImageController.store')
+}).middleware(['auth', 'adminAuth'])
