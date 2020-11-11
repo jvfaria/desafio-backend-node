cd659698b8fa4575538e766c5b5d85817e1a8263
JVitor22 <jvfaria015@gmail.com>
2020-11-10 22:04:29 -0300

    filter user show sales hook

--- desafio-backend-node/app/Middleware/UserGetSale.js
+++ desafio-backend-node/app/Middleware/UserGetSale.js
@@ -2,20 +2,19 @@
 /** @typedef {import('@adonisjs/framework/src/Request')} Request */
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
 /** @typedef {import('@adonisjs/framework/src/View')} View */
-const Sale = use('App/Models/Sale')
 class UserGetSale {
-  async handle ({ request, auth, response }, next) {
+  async handle ({ request, auth, response, params }, next) {
     try {
       await auth.check()
       const user = await auth.getUser()
-      if (user.type === 'client') {
-        const sales = await Sale
-          .query()
-          .with('products')
-          .where('user_id', user.id)
-          .fetch()
-        return response.status(200).json(sales)
+      request.user = user
+      const { from, to } = request.all()
+      if (from && to) {
+        request.filter = true
+        request.from = from
+        request.to = to
       }
+
       await next()
     } catch (error) {
       return response.status(200).json(error.messages)
